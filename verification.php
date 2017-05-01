9bec9dff8f5bc30ec897c1ddabc336f37808b458
I SLOAN <ishaqsloan@gmail.com>
2017-04-30 23:21:33 -0400

    Insert statement prep

--- verification.php
+++ verification.php
@@ -2,6 +2,8 @@
 $servername = "localhost";
 $username = "herm";
 $password = "nagrus9";
+$dbname = "hermDB";
+$tblName = "Users"
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
@@ -11,4 +13,10 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
+
+//Insert statement 
+$insertUser = "INSERT INTO " . $tblName . " VALUES ("./*Insert Form Data Here*/.")"; 
+//TODO create a function to be used upon pressing the submit button. 
+
+$conn -> close();
 ?>
